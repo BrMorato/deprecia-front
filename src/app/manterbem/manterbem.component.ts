d68588a40b6777dc31cec8b6bbb3d8a275fd1cf0
fmmachado10 <fabricio.machado@tcm.go.gov.br>
2019-12-04 18:16:17 -0300

    Melhorias

--- src/app/manterbem/manterbem.component.ts
+++ src/app/manterbem/manterbem.component.ts
@@ -15,12 +15,12 @@ export class ManterbemComponent implements OnInit {
 
   listaCrfb: Crfb[] = [];
 
-  
+
   Obj: Bem= {id:0, codigo:'', nome:'' ,descricao:'', classificacao: new Crfb, valor_aquisicao:0.00, valor_venda:0.00, estado_aquisicao:false,turnos:0,status:false, dt_aquisicao: new(Date), dt_venda: new(Date)}
-  
+
   ObjCrfb: Crfb={id:0,ref_ncm:'',descricao:'',vida_util:0,taxa_depreciacao:0.00}
-  
-  
+
+
 
   msg = '';
   constructor(private api: ManterbemService) { }
@@ -29,13 +29,13 @@ export class ManterbemComponent implements OnInit {
     this.consultarBem();
     this.consultarCrfb();
   }
-  
-  
 
-  
+
+
+
 consultarBem(){
   this.api.consultarBem().toPromise().then(res=>{this.listaBem=res;});
-  
+
   }
 
   /*consultarCrfbPorId(){
@@ -57,15 +57,18 @@ consultarBem(){
     .then(Bem => {this.msg="Bem excluído com sucesso!";
     this.consultarBem()});
   }
-  
+
   alterarBem(){
     this.api.alterarBem(this.Obj.id,this.Obj).toPromise()
     .then(Bem =>{this.msg="Bem " +Bem.nome+" atualizada com sucesso!";this.consultarBem()});
   }
-  
+
   carregarDadosBem(c: Bem){
   this.Obj = c;
   }
-  
+
+  compararClassificacao(a, b) {
+      return a.id === b.id;
+  }
 
 }
